4049a114f47216ae487f77098a06887be9b3bdac
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2021-11-22 20:16:30 +0100

    Add files via upload
    test

---
+++ src/test/java/com/pokemonpedia/baseexppokeservice/controller/BaseExpPokemonControllerTest.java
@@ -0,0 +1,40 @@
+package com.pokemonpedia.baseexppokeservice.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.pokemonpedia.baseexppokeservice.AbstractTest;
+import com.pokemonpedia.baseexppokeservice.dto.Pokemon;
+
+public class BaseExpPokemonControllerTest extends AbstractTest {
+	
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+	
+	@Test
+	public void createPokemon() throws Exception {
+		String uri = "/baseExpPokemon/saveBaseExpPokemon/";
+		Pokemon c = new Pokemon();
+		c.setId((long) 1);
+		c.setName("Gabriel");
+		c.setHeight(10);;
+		c.setWeight(50);;
+		c.setBase_experience(120);;
+		String inputJson = super.mapToJson(c);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	
+	}
+}
